bcd84611ac55bad603c8609320d5a0144b08d7b3
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-05-16 16:45:55 +0100

    removed half moon SVG from html and copied relevant code from it to the moonPath Const

---
+++ app.js
@@ -0,0 +1,3 @@
+/* I have copied the below from the original moon SVG I created in Figma, removed the rest of the SVG code from the HTML file and created the below Const variable ready for it's function */
+
+const moonPath = "M17 27.5C17 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 17 12.3122 17 27.5Z"
